77b0a52f4e38f43177eb40e9593bbef75d377164
Noel Anstey <anstey.ng@gmail.com>
2017-03-23 15:08:28 -0700

    connected mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,32 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweets").insertOne( newTweet );
+      callback(null, true);
+      // simulateDelay(() => {
+      //   db.tweets.push(newTweet);
+      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      // console.log(db.collection('tweets').find({}).toArray((err, tweets) => {return tweets}));
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   // db.tweets.find().toArray((err, tweets) => {
+      //   //   if (err) {
+      //   //     return callback(err);
+      //   //   }
+      //   //   callback(null, tweets);
+      //   // });
+      //   return [];
+      // });
+      db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
     }
 
   };
